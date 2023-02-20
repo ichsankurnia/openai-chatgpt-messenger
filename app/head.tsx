fa03e03dd9040c5d5002e47cbe309eb9354432df
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2023-02-20 12:53:09 +0700

    update head

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,16 @@
 export default function Head() {
   return (
     <>
-      <title>ChatGPT Clone</title>
+      <title>Ories OpenAI</title>
+      <meta name="theme-color" content="#000000" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="description"
+        content="OpenAI ChatGPT Messenger apps by ichsankurnia"
+      />
+      <meta name='author' content='ichsankurnia' />
+      <meta name='keywords' content='openai, chatgpt, next.js13, next.js, react.js, nextauth, tailwindcss, typescript, firebase' />
     </>
   )
 }
