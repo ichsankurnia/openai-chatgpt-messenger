92b54c523753d1448aa44ff7d5bd0565280ba405
Ichsan Kurniawan <43689091+ichsankurnia@users.noreply.github.com>
2023-02-20 12:54:47 +0700

    add thumbnail

--- app/head.tsx
+++ app/head.tsx
@@ -1,16 +1,17 @@
 export default function Head() {
   return (
     <>
-      <title>Ories OpenAI</title>
-      <meta name="theme-color" content="#000000" />
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon.ico" />
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="theme-color" content="#000000" />
       <meta
         name="description"
         content="OpenAI ChatGPT Messenger apps by ichsankurnia"
       />
       <meta name='author' content='ichsankurnia' />
       <meta name='keywords' content='openai, chatgpt, next.js13, next.js, react.js, nextauth, tailwindcss, typescript, firebase' />
+      <link rel="apple-touch-icon" href="/favicon.ico" />
+      <title>Ories OpenAI</title>
     </>
   )
 }
